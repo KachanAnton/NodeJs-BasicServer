8cad521e4c70019dd3b061675e13e92f8f8e0e31
Anton Kachan <anton.kachan@monostacklabs.com>
2017-09-13 12:20:18 +0300

    Added redirector

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var port = 4080;
 var server = require('./basic_server/basic_server').createServer();
 server.userFavIcon('localhost', "./public/favicon.png");
+server.addContainer(".*", "/l/(.*)$", require('./redirector', {}));
 server.docroot('localhost', "/", "./views");
 require('./http_sniffer').sniffOn(server);
 server.listen(port);
