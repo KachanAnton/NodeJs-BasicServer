95232ee3a04528720212f52708b9c661bbd184b5
Anton Kachan <anton.kachan@monostacklabs.com>
2017-09-13 11:13:38 +0300

    Added baseserver

---
+++ server.js
@@ -0,0 +1,6 @@
+var port = 4080;
+var server = require('./basic_server/basic_server').createServer();
+server.userFavIcon('localhost', "./public/favicon.png");
+server.docroot('localhost', "/", "./views");
+require('./http_sniffer').sniffOn(server);
+server.listen(port);
